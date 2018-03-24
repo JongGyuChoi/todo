71c2e9e4e81c5da93b7f719dade14da205848b3d
unknown <chlchl5745@gmail.com>
2018-03-24 10:32:13 +0900

    todo

---
+++ src/todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class todo extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default todo;
